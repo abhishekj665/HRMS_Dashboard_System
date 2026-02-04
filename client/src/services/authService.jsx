3ddce1bea8aefc908250b7cd361549f91e02c1f6
abhishekj665 <jeveneabhi665@gmail.com>
2026-02-04 17:15:45 +0530

    fix: remove localhost references from API and CORS configurations

--- client/src/services/authService.jsx
+++ client/src/services/authService.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:import.meta.env.VITE_BASE_URL,
 });
 
-//import.meta.env.VITE_BASE_URL
+
 
 
 
