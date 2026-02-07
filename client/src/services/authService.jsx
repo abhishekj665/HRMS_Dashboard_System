375b2b29f6fbd251f5bea5c711096764b08941f4
abhishekj665 <jeveneabhi665@gmail.com>
2026-02-07 16:59:54 +0530

    fix: update API base URL to use environment variable and remove localhost references

--- client/src/services/authService.jsx
+++ client/src/services/authService.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const API = axios.create({
-  baseURL:"http://localhost:3000",
+  baseURL:import.meta.env.VITE_BASE_URL,
 });
 
 
