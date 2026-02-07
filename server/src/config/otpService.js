b59db5b5084c2d793a2d64665fdec40e30437cdc
abhishekj665 <jeveneabhi665@gmail.com>
2026-02-07 12:04:25 +0530

    fix: improve error handling in OTP email sending process

--- server/src/config/otpService.js
+++ server/src/config/otpService.js
@@ -3,15 +3,12 @@ import { generateHash } from "../utils/hash.utils.js";
 import { env } from "./env.js";
 import * as brevo from "@getbrevo/brevo";
 
-
 const apiInstance = new brevo.TransactionalEmailsApi();
 
 apiInstance.setApiKey(
   brevo.TransactionalEmailsApiApiKeys.apiKey,
-  env.brevo_api_key
+  env.brevo_api_key,
 );
-
-
 
 export const createOTP = async (email, otp, purpose) => {
   const hashedOtp = await generateHash(otp);
@@ -22,7 +19,9 @@ export const createOTP = async (email, otp, purpose) => {
     purpose,
   });
 
-  sendMailOtp(email, otp);
+  sendMailOtp(email, otp).catch((err) => {
+    console.error("OTP MAIL ERROR:", err.response?.body || err.message);
+  });
 };
 
 export const sendMailOtp = async (email, otp) => {
@@ -34,23 +33,26 @@ export const sendMailOtp = async (email, otp) => {
   await sendMail(email, "OTP Verification", html);
 };
 
-
-
 export async function sendMail(to, subject, html) {
-  const mail = new brevo.SendSmtpEmail();
+  try {
+    const mail = new brevo.SendSmtpEmail();
 
-  mail.to = [{ email: to }];
-  mail.subject = subject;
-  mail.htmlContent = html;
-  mail.sender = {
-    name: "Dashboard System",
-    email: env.mail_user, 
-  };
+    mail.to = [{ email: to }];
+    mail.subject = subject;
+    mail.htmlContent = html;
+    mail.sender = {
+      name: "Dashboard System",
+      email: env.mail_user,
+    };
 
-  return apiInstance.sendTransacEmail(mail);
+    const res = await apiInstance.sendTransacEmail(mail);
+    console.log("BREVO SENT:", res.body?.messageId);
+    return res;
+  } catch (err) {
+    console.error("BREVO FAIL:", err.response?.body || err.message);
+    throw err;
+  }
 }
-
-
 
 export const findOtpData = async (email, purpose) => {
   return OTP.findOne({
