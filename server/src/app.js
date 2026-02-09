9cd2e73c85d993c910925fefccec2089f7b9f69d
abhishekj665 <jeveneabhi665@gmail.com>
2026-02-09 12:39:25 +0530

    fix: set trust proxy in app configuration and simplify cookie setting function

--- server/src/app.js
+++ server/src/app.js
@@ -29,7 +29,7 @@ app.use(
     credentials: true,
   }),
 );
-
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- server/src/services/cookie.service.js
+++ server/src/services/cookie.service.js
@@ -5,13 +5,11 @@ export const clearCookie = (res, name) => {
   });
 };
 
-export const setCookie = (res, name, value, options = {}) => {
+export const setCookie = (res, name, value) => {
   res.cookie(name, value, {
     httpOnly: true,
     secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    ...options
   });
 };
-
