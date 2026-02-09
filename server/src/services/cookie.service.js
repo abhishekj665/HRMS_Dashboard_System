c33f3b0a942c2522583561c7fa875548ad8d8ae4
abhishekj665 <jeveneabhi665@gmail.com>
2026-02-09 12:57:52 +0530

    fix: refactor clearCookie function to use shared cookieOptions for consistency

--- server/src/services/cookie.service.js
+++ server/src/services/cookie.service.js
@@ -1,17 +1,18 @@
-export const clearCookie = (res, name) => {
-  res.clearCookie(name, {
-    httpOnly: true,
-    sameSite: "none",
-    secure : true,
-    path : "/"
-  });
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  path: "/",
 };
 
 export const setCookie = (res, name, value) => {
   res.cookie(name, value, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    ...cookieOptions,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
+
+export const clearCookie = (res, name) => {
+  res.clearCookie(name, cookieOptions);
+};
+
