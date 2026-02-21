fa5d2699a5654637024f93b4c3a1798892a6ac99
abhishekj665 <jeveneabhi665@gmail.com>
2026-02-21 16:44:46 +0530

    removing extra and redundant file

--- server/src/routes/lms/lms.route.js
+++ server/src/routes/lms/lms.route.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { leaveBalanceRouter } from "./leaveBalance.route.js";
-import { leaveRequestRouter } from "./leaveRequest.route.js";
 import { leaveTypeRouter } from "./leaveType.route.js";
 import { leavePolicyRouter } from "./leavePolicy.route.js";
 
@@ -8,7 +7,6 @@ import { leavePolicyRouter } from "./leavePolicy.route.js";
 const Router = express.Router();
 
 Router.use(leaveBalanceRouter);
-Router.use(leaveRequestRouter);
 Router.use(leaveTypeRouter);
 Router.use(leavePolicyRouter);
 
