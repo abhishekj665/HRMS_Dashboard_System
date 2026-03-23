6940bc4d6271c77e8ff18e1ecb7d27ab60fcd782
abhishekj665 <jeveneabhi665@gmail.com>
2026-03-23 19:01:00 +0530

    change in the route to validate with adminAuth middleware after refresh auth middleware

--- server/src/routes/recruitment/hiringStage.route.js
+++ server/src/routes/recruitment/hiringStage.route.js
@@ -7,8 +7,8 @@ const Router = express.Router();
 
 Router.patch(
   "/stage/next/:applicationId",
-  adminAuth,
   refreshAuth,
+  adminAuth,
   HiringStageController.moveToNextStage,
 );
 export const HiringStageRoute = Router;
