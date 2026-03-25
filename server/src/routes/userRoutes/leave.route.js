4c17b6b7d18c9379f8600ea936c5e6b610d1a969
abhishekj665 <jeveneabhi665@gmail.com>
2026-03-26 05:26:38 +0530

    refactor: Update leave routes to use userAuth middleware instead of allowRoles

--- server/src/routes/userRoutes/leave.route.js
+++ server/src/routes/userRoutes/leave.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import * as userLeaveController from "../../controllers/user/leave.controller.js";
-import { allowRoles } from "../../middlewares/roleAuth.middleware.js";
+import { allowRoles , userAuth} from "../../middlewares/roleAuth.middleware.js";
 
 
 const Router = express.Router();
 
 
 
-Router.get("/lms/leave/requests",allowRoles("manager", "admin", "employee"), userLeaveController.getLeaveRequests);
-Router.get("/lms/leave/leave-balance",allowRoles("manager", "admin", "employee"), userLeaveController.getLeaveBalance);
+Router.get("/lms/leave/requests",userAuth, userLeaveController.getLeaveRequests);
+Router.get("/lms/leave/leave-balance",userAuth, userLeaveController.getLeaveBalance);
 
-Router.post("/lms/leave/apply",allowRoles("manager", "admin", "employee"), userLeaveController.registerLeaveRequest);
+Router.post("/lms/leave/apply",userAuth, userLeaveController.registerLeaveRequest);
 
 export const userLMSRouter = Router;
